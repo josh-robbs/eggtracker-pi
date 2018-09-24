ae8a4f52231c3c3c9f057d4e5cf70e8cf2dfa3b4
Josh Robbs <joshrobbs@Zuul.local>
2018-09-24 11:11:37 -0600

    more testing

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 console.log('taco test')
 
-fetch('https://eggtrackerapp.herokuapp.com/foodstuff',{
-  method: 'POST',
-  headers: {
-    "content-type": "application/json"
-    },
-  body: { "category": "test pi",
-          "brand": "raspbi pi",
-          "name": "taco test",
-          "img": "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-          "quantity": 666,
-          "location": "refrigerator" }
-  })
-  .then(res => res.status=201 ? this.setRedirect() : console.log('not working'))
+function getStuff(){
+  fetch('https://eggtrackerapp.herokuapp.com/foodstuff',{
+    method: 'POST',
+    headers: {
+      "content-type": "application/json"
+      },
+    body: { "category": "test pi",
+            "brand": "raspbi pi",
+            "name": "taco test",
+            "img": "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "quantity": 666,
+            "location": "refrigerator" }
+    })
+    .then(res => res.status=201 ? this.setRedirect() : console.log('not working'))
+}
+
+getStuff()
