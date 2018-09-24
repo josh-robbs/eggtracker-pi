fd4fb3675a3e3c22798f609a59ff6bd278581b98
Josh Robbs <joshrobbs@Zuul.local>
2018-09-24 11:10:11 -0600

    still testing

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-console.log('taco')
-
-let body = {  "category": "test pi",
-              "brand": "raspbi pi",
-              "name": "taco test",
-              "img": "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-              "quantity": 666,
-              "location": "refrigerator" }
+console.log('taco test')
 
 fetch('https://eggtrackerapp.herokuapp.com/foodstuff',{
   method: 'POST',
   headers: {
     "content-type": "application/json"
     },
-  body: body
-    })
+  body: { "category": "test pi",
+          "brand": "raspbi pi",
+          "name": "taco test",
+          "img": "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+          "quantity": 666,
+          "location": "refrigerator" }
+  })
   .then(res => res.status=201 ? this.setRedirect() : console.log('not working'))
