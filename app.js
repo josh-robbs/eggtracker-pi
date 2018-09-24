97e7cffdfc1fb6aae8eac211aac23f6882d6e6ff
Josh Robbs <joshrobbs@Zuul.local>
2018-09-24 12:06:08 -0600

    stringify add

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const fetch = require("node-fetch")
 
 console.log('taco test')
 
+var body = JSON.stringify({  category: "test pi",
+              brand: "raspbi pi",
+              name: "taco test",
+              img: "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+              quantity: 666,
+              location: "refrigerator" })
+
 function getStuff(){
   console.log('comment inside fetch')
   fetch('https://eggtrackerapp.herokuapp.com/foodstuff',{
     method: 'POST',
     headers: { "content-type": "application/json" },
-    body: { "category": "test pi",
-            "brand": "raspbi pi",
-            "name": "taco test",
-            "img": "https://images.pexels.com/photos/38283/bananas-fruit-carbohydrates-sweet-38283.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            "quantity": 666,
-            "location": "refrigerator" }
+    body: body
     })
     .then(res => res.status=201 ? console.log(res.status) : console.log('not working'))
 }
